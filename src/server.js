3479937625f92460c0343b4788b0708a5cefef3f
salim-maula <huzaifah956@gmail.com>
2021-07-11 16:56:04 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,36 @@
+const Hapi = require('@hapi/hapi');
+// const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
+
+require('dotenv').config();
+
+const init = async () => {
+  const notesService = new NotesService();
+
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  // server.route(routes);
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
